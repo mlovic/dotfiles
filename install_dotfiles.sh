8aa251320302f1f2ce993cd0b76a2ba1b2551e15
Your Name <you@example.com>
2024-12-15 08:12:14 -0800

    rm bad line

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -97,6 +97,5 @@ done
 
 # Make scripts executable
 chmod +x "$HOME/.config/i3/toggle_keyboard_layout.sh"
-chmod +x "$DOTFILES_DIR/scripts/setup-server.sh"
 
 log "Installation complete! Please restart your shell."
